aa5ef74e7dcfc7b4553667fb5bc4c38dda119d5a
clement-soler <clement.soler@hetic.net>
2016-11-29 00:13:24 +0100

    little bug with post create -> continue and resolve tomorrow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def index
+     @posts = Post.all
   end
 
   def new
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
   end
 
   private
+  
   def post_params
     params.require(:post).permit(:image, :caption)
   end
