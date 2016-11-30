b253407dcd469772bce09a0feb58678fdca9dd7c
clement-soler <clement.soler@hetic.net>
2016-11-30 23:07:29 +0100

    add comment controller + crud method and associate comment to user + style for comments - work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, dependent: :destroy # dependent user destroy, his comment too
 
   validates :user_id, presence: true
   validates :image, presence: true
 
-  has_attached_file :image, styles: { :medium => "640x" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { :medium => "640x" } # image size
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ # image format jpg png ect ...
 end
