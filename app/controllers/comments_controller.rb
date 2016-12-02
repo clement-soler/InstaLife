c119baee966fc0fac395589bdf50df5fc4fca198
clement-soler <clement.soler@hetic.net>
2016-12-02 10:06:06 +0100

    add posted at (time) + ajax for comment - create using jquery and remote true on form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,12 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "Your comment has been published."
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
-      flash[:alert] = "Oups... please check the form."
+      flash[:alert] = "Check the comment form, something went wrong."
       render root_path
     end
   end
