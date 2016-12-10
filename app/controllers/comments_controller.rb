70a018fa3dfe4d26a5e1e24d6bbd82d89150f26d
clement-soler <clement.soler@hetic.net>
2016-12-10 22:11:15 +0100

    add create notification method to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,11 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
+          create_notification @post, @comment
+          respond_to do |format|
+            format.html { redirect_to root_path }
+            format.js
+          end
     else
       flash[:alert] = 'Check the comment form, something went wrong.'
       render root_path
@@ -37,6 +38,11 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def create_notification(post)
+      return if post.user.id == current_user.id
+      Notification.create(user_id: post.user.id, notified_by_id: current_user.id, post_id: post.id,comment_id: comment.id,notice_type: 'comment')
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
