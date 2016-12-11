e04ef2445ee3e4b1d58037cc6a94b000132b3b00
clement-soler <clement.soler@hetic.net>
2016-12-11 22:56:23 +0100

    create user seeder to test futur follow features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create :user_name => "User1", :email => "user1@example.com", :password => "localtest"
+user2 = User.create :user_name => "User2", :email => "user2@example.com", :password => "localtest"
+user3 = User.create :user_name => "User3", :email => "user3@example.com", :password => "localtest"
